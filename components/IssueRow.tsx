4f9c0a99f3879c7ba77b7cbfb6a0da35fc993846
Daniel Kuroski <daniel.kuroski@gmail.com>
2022-06-25 21:04:25 +0200

    WIP: IssueRow component

--- components/IssueRow.tsx
+++ components/IssueRow.tsx
@@ -1,13 +1,13 @@
-import { Box, ColorProps, Flex, Heading, Link, Text } from "@chakra-ui/react";
-import { CheckCircledIcon, CircleIcon, DotFilledIcon, DotIcon, Link1Icon } from "@radix-ui/react-icons";
+import { AvatarGroup, Box, Flex, Heading, Link, Text, Tooltip } from "@chakra-ui/react";
+import { ChatBubbleIcon, CheckCircledIcon, CircleIcon, CommitIcon, DotFilledIcon } from "@radix-ui/react-icons";
 import NextLink from "next/link";
-import Trans from "next-translate/Trans"
+import Trans from "next-translate/Trans";
 import useTranslation from "next-translate/useTranslation";
 
+import TooltipAvatar from "@/components/TooltipAvatar";
+import UserLink from "@/components/UserLink";
 import { IssueDecoder } from "@/lib/decoders/issue";
 import { dateTimeFormat } from "@/lib/utils";
-
-import UserLink from "./UserLink";
 
 type IssueSubtitleProps = IssueDecoder
 const IssueSubtitle = (props: IssueSubtitleProps) => {
@@ -55,39 +55,63 @@ type IssueRowProps = {
 }
 
 const IssueRow = (props: IssueRowProps) => {
+  const { t } = useTranslation('common')
   return (
     <Flex alignItems="center" gap="2">
       <>
         {props.issue.state === "open" && (
-          <Box position="relative" color="green.500" alignSelf="flex-start">
-            <CircleIcon width="24" height="24" />
-            <DotFilledIcon style={{
-              position: "absolute",
-              top: "50%",
-              left: "50%",
-              transform: "translate(-50%, -50%)",
-            }} width="16" height="16" />
-          </Box>
+          <Tooltip label={t('open_issue')} openDelay={500}>
+            <Box position="relative" color="green.500" alignSelf="flex-start">
+              <CircleIcon width="24" height="24" />
+              <DotFilledIcon style={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+              }} width="16" height="16" />
+            </Box>
+          </Tooltip>
         )}
         {props.issue.state === "closed" && (
-          <Box color="purple.500" alignSelf="flex-start">
-            <CheckCircledIcon width="24" height="24" />
-          </Box>
+          <Tooltip label={t('closed_issue')} openDelay={500}>
+            <Box color="purple.500" alignSelf="flex-start">
+              <CheckCircledIcon width="24" height="24" />
+            </Box>
+          </Tooltip>
         )}
 
 
         <Flex flex="1" direction="column" gap="1">
           <Heading as="h3" size="sm">
-            <NextLink href={{ pathname: "/issues/:id" }} passHref>
-              <Link>{props.issue.title}</Link>
+            <NextLink href={{ pathname: "/issues/[id]", query: { id: props.issue.number } }} passHref>
+              <Link _hover={{ color: "blue.500" }}>{props.issue.title}</Link>
             </NextLink>
           </Heading>
           <IssueSubtitle {...props.issue} />
         </Flex>
 
-        <Link href={props.issue.url} target="_blank">
-          <Link1Icon />
-        </Link>
+        <Flex alignSelf="flex-start" alignItems="center" gap="6">
+          {props.issue.pullRequest && (
+            <Link href={props.issue.pullRequest.htmlUrl} target="_blank" _hover={{ color: "blue.500" }} display="flex" alignItems="center" gap="1">
+              <CommitIcon />
+            </Link>
+          )}
+          {props.issue.assignees && (
+            <AvatarGroup size='xs' max={3}>
+              {props.issue.assignees.map((assignee) => (
+                <TooltipAvatar key={assignee.id} name={t('assigned_to', { user: assignee.login })} as={Link} src={assignee.avatarUrl} href={assignee.htmlUrl} target="_blank" />
+              ))}
+            </AvatarGroup>
+          )}
+          {props.issue.comments > 0 && (
+            <NextLink href={{ pathname: "/issues/[id]", query: { id: props.issue.number } }} passHref>
+              <Link _hover={{ color: "blue.500" }} display="flex" alignItems="center" gap="1">
+                <ChatBubbleIcon />
+                {props.issue.comments}
+              </Link>
+            </NextLink>
+          )}
+        </Flex>
       </>
     </Flex>
   )
