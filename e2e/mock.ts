ca608f4c46d28874d26ef9e95a5688b76dacce36
Daniel Kuroski <daniel.kuroski@gmail.com>
2022-07-31 16:52:34 +0200

    Fix mock functions for tests

--- e2e/mock.ts
+++ e2e/mock.ts
@@ -1,21 +1,34 @@
 import { generateMock } from '@anatine/zod-mock';
-import { rest } from 'msw'
-import { setupServer } from 'msw/node'
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import snakecaseKeys from 'snakecase-keys';
 
 import { issuesResponseDecoder } from '@/lib/decoders/issue';
+import { orgsResponseDecoder } from '@/lib/decoders/org';
+import { reposResponseDecoder } from '@/lib/decoders/repo';
+import { dateFrom } from '@/lib/utils';
 
+const snakeCaseObject = <T>(obj: T) => snakecaseKeys(obj, { deep: true })
+const options = {
+  stringMap: {
+    created_at: () => dateFrom((new Date()).toISOString()).toString(),
+    closed_at: () => dateFrom((new Date()).toISOString()).toString(),
+    updated_at: () => dateFrom((new Date()).toISOString()).toString(),
+  }
+}
 const handlers = [
-  // rest.all('https://api.github.com*', (_req, res, ctx) => {
-  //   return res(
-  //     ctx.status(500),
-  //     ctx.json({ error: 'Oops' })
-  //   )
-  // }),
-
   rest.get('https://api.github.com/user/issues', (_req, res, ctx) => res(
     ctx.status(200),
-    ctx.json(generateMock(issuesResponseDecoder))
-  ))
+    ctx.json(generateMock(issuesResponseDecoder.transform(snakeCaseObject), options))
+  )),
+  rest.get('https://api.github.com/user/orgs', (_req, res, ctx) => res(
+    ctx.status(200),
+    ctx.json(generateMock(orgsResponseDecoder.transform(snakeCaseObject), options))
+  )),
+  rest.get('https://api.github.com/user/repos', (_req, res, ctx) => res(
+    ctx.status(200),
+    ctx.json(generateMock(reposResponseDecoder.transform(snakeCaseObject), options))
+  )),
 ]
 
 const mockServer = setupServer(...handlers)
